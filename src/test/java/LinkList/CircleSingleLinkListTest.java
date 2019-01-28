c6000ef835a24702f52e27e460590b4880ec0bf7
jiangyusheng <jiangyusheng@WINDOWS-FPIDAS5>
2019-01-28 18:42:19 +0800

    Add Test Class

---
+++ src/test/java/LinkList/CircleSingleLinkListTest.java
@@ -0,0 +1,28 @@
+package LinkList;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CircleSingleLinkListTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass(){
+
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void hasCircle() {
+    }
+
+    @Test
+    public void circleLength() {
+    }
+}
---
+++ src/test/java/LinkList/ReverseSingleLinkListTest.java
@@ -0,0 +1,12 @@
+package LinkList;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReverseSingleLinkListTest {
+
+    @Test
+    public void reverse() {
+    }
+}
